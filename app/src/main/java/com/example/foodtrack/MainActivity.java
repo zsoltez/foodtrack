63411012ef85d35778d20d00c95bd5b1a080b564
Szente Zsolt <h269508@stud.u-szeged.hu>
2025-04-10 22:54:41 +0200

    MainActivity UI, RegisterActivity UI, MainActivity Logo Animation

--- app/src/main/java/com/example/foodtrack/MainActivity.java
+++ app/src/main/java/com/example/foodtrack/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.foodtrack;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private static final String SECRET_KEY = "h7pACEh9MXz2";
+    private final Handler handler = new Handler();
+    private Runnable scaleLogoRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +28,68 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        ViewCompat.setOnApplyWindowInsetsListener(toolbar, (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
 
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        // Logo animáció
+        Animation scaleLogoAnim = AnimationUtils.loadAnimation(this, R.anim.logo_scale);
+        scaleLogoRunnable = () -> {
+            logoImageView.startAnimation(scaleLogoAnim);
+//            handler.postDelayed(scaleLogoRunnable, 5000); // újra fusson 5 mp múlva
+        };
+
+    }
+
+    public void login(View view) {
+        // TODO
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_menu, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+        handler.removeCallbacks(scaleLogoRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+        handler.postDelayed(scaleLogoRunnable, 1000);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
     }
 }
---
+++ app/src/main/java/com/example/foodtrack/RegisterActivity.java
@@ -0,0 +1,45 @@
+package com.example.foodtrack;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
+    private static final String SECRET_KEY = "h7pACEh9MXz2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String secret_key = getIntent().getStringExtra("SECRET_KEY");
+        if(!secret_key.equals(SECRET_KEY)){
+            finish();
+        }
+
+    }
+
+    public void register(View view) {
+        // TODO
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+}
